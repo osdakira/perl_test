ac7e279c2535795d6fc3946899adbf6053eee843
Akira Osada <osd.akira@gmail.com>
2016-10-08 16:29:30 +0900

    Add config with plugin

--- othello_web/lib/Othello/Web.pm
+++ othello_web/lib/Othello/Web.pm
@@ -12,6 +12,9 @@ sub startup {
   $self->asset->process('base.css' => ('sass/base.sass'));
   # $self->secrets(["secrets_key"]);
 
+  $self->plugin('DefaultHelpers');
+  $self->plugin('Mojolicious::Plugin::Config');
+
   # Router
   my $r = $self->routes;
 
--- othello_web/lib/Othello/Web/Controller/Example.pm
+++ othello_web/lib/Othello/Web/Controller/Example.pm
@@ -4,12 +4,15 @@ use Othello::Service::Container qw/container/;
 
 # This action will render a template
 sub welcome {
-  my $self = shift;
+    my $self = shift;
 
-  $self->app->log->debug("debug test");
-  # Render template "example/welcome.html.ep" with message
-  # $self->app->log->debug(container->get("ui_conf"));
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+    $self->app->log->debug($self->app->dumper($self->app->config()));
+    $self->app->log->debug($self->app->dumper($self->app->moniker));
+
+    $self->app->log->debug("debug test");
+    # Render template "example/welcome.html.ep" with message
+    # $self->app->log->debug(container->get("ui_conf"));
+    $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
 }
 
 sub post_test {
